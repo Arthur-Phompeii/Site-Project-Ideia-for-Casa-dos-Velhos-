5ecef03fbca3b1a78ce654b88df1c818f2510f07
Arthur Esteves <freitasestevesarthur@gmail.com>
2026-09-19 18:35:41 -0300

    Delete Programação de Computadores - CS directory

--- Programação de Computadores - CS/Atividade Prática 1/calculo_fatorial.py
+++
@@ -1,9 +0,0 @@
-import math
-
-while True:
-    try:
-        fatorial = int(input("Cálculo de Fatorial (Escolha um número de 1 a 10):")) # A conversão é feita diretamente na coleta de dados pelo input
-        resultado_fatorial = math.factorial(fatorial)
-        print(f"O resultado do fatorial de {fatorial}! é {resultado_fatorial}.")
-    except ValueError:
-        print("Digite apenas números inteiros!")
